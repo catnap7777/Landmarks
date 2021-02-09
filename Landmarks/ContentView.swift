152f408c69f4b1e5a6018132b736a79ad4e29f2e
km <35552602+catnap7777@users.noreply.github.com>
2021-02-09 09:01:21 -0600

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Karen Mathes on 2/9/21.
+//  Copyright © 2021 TygerMatrix Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
